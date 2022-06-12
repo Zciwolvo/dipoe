9db64eb5e11c3c87f68316ab028df39d924a5490
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2022-06-12 23:19:13 +0200

    Updates

---
+++ precache-manifest.9162b3960c6c7ec0c05770809aa4d799.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36acf3ed6010fe0e8973c7d2e6c9e670",
+    "url": "/dipoe/index.html"
+  },
+  {
+    "revision": "f17a55cbd7e042d99476",
+    "url": "/dipoe/static/js/2.0e926c68.chunk.js"
+  },
+  {
+    "revision": "151a90b69bcc6fc8a12c8391747d6069",
+    "url": "/dipoe/static/js/2.0e926c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6409938446fdc095df64",
+    "url": "/dipoe/static/js/main.e0ada331.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/dipoe/static/js/main.e0ada331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891969a5408f4f6989dd",
+    "url": "/dipoe/static/js/runtime-main.8ccc9d23.js"
+  },
+  {
+    "revision": "2039ea26278437d665950e2b7fee3f70",
+    "url": "/dipoe/static/media/background2.2039ea26.png"
+  },
+  {
+    "revision": "d601e2fc6c330fb43cc5ab3c00b9b25c",
+    "url": "/dipoe/static/media/disc.d601e2fc.jpg"
+  },
+  {
+    "revision": "b946e774925506dd75c828d9a5bf2ca0",
+    "url": "/dipoe/static/media/logo.b946e774.png"
+  }
+]);
