2645a297b04012c3859a148ee27530b249b4a939
Zciwolvo <igor.gawlowicz@gmail.com>
2022-06-15 14:32:51 +0200

    u

--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -12,7 +12,7 @@ const Order = styled.form`
     0px 2px 5px 0px rgba(50, 50, 93, 0.1), 0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
   border-radius: 7px;
   padding: 40px;
-  background-color: white;
+  background-color: black;
   @media (max-width: 720px) {
     width: 90vw;
     min-width: 0;
@@ -37,6 +37,8 @@ const Input = styled.input`
   width: 100%;
   height: 3em;
   padding-left: 1em;
+  background-color: grey;
+  color: white;
   @media (max-width: 720px) {
     width: 80vw;
     min-width: 0;
@@ -56,7 +58,7 @@ const InputText = styled.p`
   height: auto;
   font-family: Arial, sans-serif;
   font-size: 1.5em;
-  color: black;
+  color: white;
   margin: 0;
 `;
 
@@ -70,7 +72,7 @@ const Row = styled.div`
 `;
 
 const PaymentButton = styled.button`
-  background: #5469d4;
+  background: orangered;
   font-family: Arial, sans-serif;
   color: #ffffff;
   border-radius: 4px;
