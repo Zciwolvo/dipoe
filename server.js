e1d91577629903bd4b1f6e0c93a748551bf99acc
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2022-06-17 00:59:10 +0200

    Deploy app to Heroku

--- server.js
+++ server.js
@@ -25,10 +25,15 @@ app.post("/create-payment-intent", async(res) => {
     });
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(
-        path.resolve(__dirname, "client", "build", "./public/index.html")
-    );
-});
+if (process.env.NODE_ENV === "production") {
+    // Exprees will serve up production assets
+    app.use(express.static("client/build"));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(port, () => console.log(`Node server listening on port ${port}!`));
