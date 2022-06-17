ac05048a8308dd0f8631642c835d40b979c67d17
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2022-06-17 11:50:51 +0200

    Deploy app to Heroku

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const stripe = require("stripe")(
     "sk_test_51L7nq8GOhLaGDHrE8NdbFkg6DlmKINK8m7RWLt02itff4JznaW736vqsrCQwIP5Qr1wm2S8XX5Amol0kQTz3pEnh002Sx9L5YB"
 );
 
+const path = require("path");
+const port = process.env.PORT || 4242;
+
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -16,9 +19,13 @@ app.post("/create-payment-intent", async(req, res) => {
         payment_method_types: ["card", "p24"],
     });
 
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname + "/index.html"));
+    });
+
     res.send({
         clientSecret: paymentIntent.client_secret,
     });
 });
 
-app.listen(4242, () => console.log("Node server listening on port 4242!"));
+app.listen(port, () => console.log(`Node server listening on port ${port}!`));
