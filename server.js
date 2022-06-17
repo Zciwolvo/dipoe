eacd627425965b004554c029d88cbc914a932dc3
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2022-06-17 12:11:37 +0200

    Deploy app to Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const stripe = require("stripe")(
     "sk_test_51L7nq8GOhLaGDHrE8NdbFkg6DlmKINK8m7RWLt02itff4JznaW736vqsrCQwIP5Qr1wm2S8XX5Amol0kQTz3pEnh002Sx9L5YB"
 );
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
 app.post("/create-payment-intent", async(req, res) => {
