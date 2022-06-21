b4b12e6619c4995e1897561b71ca8b3905d4d63a
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2022-06-21 14:55:37 +0200

    Deploy app to Heroku

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -109,7 +109,7 @@ export default function CheckoutForm(props) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://www.dipoe.pl/success",
+        return_url: "https://www.dipoe.pl/success",
       },
     });
 
