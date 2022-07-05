d3f0688a572dba559cc603744e5bad1862283481
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2022-07-05 23:00:08 +0200

    U

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -153,7 +153,7 @@ export default function CheckoutForm({ setCount }) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "https://dipoe.vercel.app/success",
+        return_url: "https://dipoe.vercel.app/payment",
       },
     });
 
