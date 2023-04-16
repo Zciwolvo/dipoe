879966bb0f4460bb6b283c36a926fc49134ae2d8
Igor Gawłowicz <igor.gawlowicz@gmail.com>
2023-04-16 17:24:07 +0200

    payment adjustments

--- src/Payment.jsx
+++ src/Payment.jsx
@@ -30,15 +30,11 @@ const stripePromise = loadStripe(stripe_public);
 export default function Payment({ setCount, price }) {
   const [clientSecret, setClientSecret] = useState("");
 
-  var allowedOrigins = [
-    "http://localhost:3000",
-    "https://dipoe.pl",
-    "https://dipoe-git-developer-zciwolvo.vercel.app",
-  ];
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     fetch(`${process.env.REACT_APP_API_ENDPOINT}create-payment-intent`, {
       method: "POST",
+      mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
       },
